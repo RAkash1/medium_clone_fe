3b69824a90cd52778db6a439d7309c4fe95703c4
Akash Raturi <akashraturi1234@gmail.com>
2024-02-04 23:30:58 +0530

    .

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {api} from "../api";
+import api from "../api";
 
 export default function Post() {
+  console.log(api);
   const params = useParams();
   const [postInfo, setPostInfo] = useState();
   useEffect(() => {
