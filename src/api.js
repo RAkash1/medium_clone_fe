e254f064b91e70e4c346a25ed951d605b520fd3a
Akash Raturi <akashraturi1234@gmail.com>
2024-02-04 20:46:28 +0530

    changing api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-// const Api = 'https://medium-clon-be.vercel.app'
-const Api = 'http://localhost:4000'
+const Api = 'https://medium-clon-be.vercel.app'
+// const Api = 'http://localhost:4000'
 export default Api;
